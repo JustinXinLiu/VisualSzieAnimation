23528b5b859ce44952d2add3d9dc0b51d106afd9
Justin <justin@drawboard.com>
2016-11-15 16:10:03 +1100

    Implicit animation implementation.

---
+++ VisualSzieAnimation/AnimationExtensions.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Numerics;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+
+namespace VisualSzieAnimation
+{
+    public static class AnimationExtensions
+    {
+        public static CompositionPropertySet ScrollProperties(this ScrollViewer scrollViewer)
+        {
+            return ElementCompositionPreview.GetScrollViewerManipulationPropertySet(scrollViewer);
+        }
+
+        public static Visual Visual(this UIElement element)
+        {
+            return ElementCompositionPreview.GetElementVisual(element);
+        }
+
+        public static ContainerVisual ContainerVisual(this UIElement element)
+        {
+            var hostVisual = ElementCompositionPreview.GetElementVisual(element);
+            var root = hostVisual.Compositor.CreateContainerVisual();
+            ElementCompositionPreview.SetElementChildVisual(element, root);
+            return root;
+        }
+
+        public static void SetChildVisual(this UIElement element, Visual childVisual)
+        {
+            ElementCompositionPreview.SetElementChildVisual(element, childVisual);
+        }
+
+        #region Composition
+
+        public static void EnableImplicitAnimation(this UIElement element, VisualPropertyType typeToAnimate,
+            double duration = 800, double delay = 0, CompositionEasingFunction easing = null)
+        {
+            var visual = element.Visual();
+            var compositor = visual.Compositor;
+
+            var animationCollection = compositor.CreateImplicitAnimationCollection();
+
+            foreach (var type in EnumExtensions.GetValues<VisualPropertyType>())
+            {
+                if (!typeToAnimate.HasFlag(type)) continue;
+
+                var animation = CreateAnimationByType(compositor, type, duration, delay, easing);
+
+                if (animation != null)
+                {
+                    animationCollection[type.ToString()] = animation;
+                }
+            }
+
+            visual.ImplicitAnimations = animationCollection;
+        }
+
+        public static void EnableImplicitAnimation(this Visual visual, VisualPropertyType typeToAnimate,
+            double duration = 800, double delay = 0, CompositionEasingFunction easing = null)
+        {
+            var compositor = visual.Compositor;
+
+            var animationCollection = compositor.CreateImplicitAnimationCollection();
+
+            foreach (var type in EnumExtensions.GetValues<VisualPropertyType>())
+            {
+                if (!typeToAnimate.HasFlag(type)) continue;
+
+                var animation = CreateAnimationByType(compositor, type, duration, delay, easing);
+
+                if (animation != null)
+                {
+                    animationCollection[type.ToString()] = animation;
+                }
+            }
+
+            visual.ImplicitAnimations = animationCollection;
+        }
+
+        private static KeyFrameAnimation CreateAnimationByType(Compositor compositor, VisualPropertyType type,
+            double duration = 800, double delay = 0, CompositionEasingFunction easing = null)
+        {
+            KeyFrameAnimation animation;
+
+            switch (type)
+            {
+                case VisualPropertyType.Offset:
+                case VisualPropertyType.Scale:
+                    animation = compositor.CreateVector3KeyFrameAnimation();
+                    break;
+                case VisualPropertyType.Size:
+                    animation = compositor.CreateVector2KeyFrameAnimation();
+                    break;
+                case VisualPropertyType.Opacity:
+                case VisualPropertyType.RotationAngleInDegrees:
+                    animation = compositor.CreateScalarKeyFrameAnimation();
+                    break;
+                default:
+                    return null;
+            }
+
+            animation.InsertExpressionKeyFrame(1.0f, "this.FinalValue", easing);
+            animation.Duration = TimeSpan.FromMilliseconds(duration);
+            animation.DelayTime = TimeSpan.FromMilliseconds(delay);
+            animation.Target = type.ToString();
+
+            return animation;
+        }
+
+        #region Cubic Easing
+
+        public static CubicBezierEasingFunction EaseInCubic(this Compositor compositor)
+        {
+            return compositor.CreateCubicBezierEasingFunction(new Vector2(0.55f, 0.055f), new Vector2(0.675f, 0.19f));
+        }
+
+        public static CubicBezierEasingFunction EaseOutCubic(this Compositor compositor)
+        {
+            return compositor.CreateCubicBezierEasingFunction(new Vector2(0.215f, 0.61f), new Vector2(0.355f, 1.0f));
+        }
+
+        public static CubicBezierEasingFunction EaseInOutCubic(this Compositor compositor)
+        {
+            return compositor.CreateCubicBezierEasingFunction(new Vector2(0.645f, 0.045f), new Vector2(0.355f, 1.0f));
+        }
+
+        #endregion
+
+        #endregion
+    }
+
+    [Flags]
+    public enum VisualPropertyType
+    {
+        None = 0,
+        Opacity = 1 << 0,
+        Offset = 1 << 1,
+        Scale = 1 << 2,
+        Size = 1 << 3,
+        RotationAngleInDegrees = 1 << 4,
+        All = ~0
+    }
+}
--- VisualSzieAnimation/MainPage.xaml.cs
+++ VisualSzieAnimation/MainPage.xaml.cs
@@ -1,30 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using System.Numerics;
+using System.Threading.Tasks;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace VisualSzieAnimation
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var rootVisual = Root.ContainerVisual();
+            var compositor = rootVisual.Compositor;
+
+            var visual = compositor.CreateSpriteVisual();
+            visual.Brush = compositor.CreateColorBrush(Colors.LightSeaGreen);
+            visual.Size = new Vector2(200.0f, 200.0f);
+            visual.Offset = new Vector3(24.0f, 24.0f, 0.0f);
+
+            visual.EnableImplicitAnimation(VisualPropertyType.All);
+
+            rootVisual.Children.InsertAtTop(visual);
+
+            await Task.Delay(3000);
+            visual.Opacity = 0.2f;
+
+            await Task.Delay(3000);
+            visual.Offset = new Vector3(120.0f, 120.0f, 0.0f);
+
+            await Task.Delay(3000);
+            visual.Size = new Vector2(400.0f, 400.0f);
         }
     }
 }
